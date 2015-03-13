9b2ba2aa4ffe971a6e34e03ecb8044ee1cfabe03
Vladislav Pugachev <vladya108@gmail.com>
2015-03-13 13:36:24 +0400

    Add ISKCON's historical events

---
+++ src/Jyotish/Calendar/IskconEvents.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @link      http://github.com/kunjara/jyotish for the canonical source repository
+ * @license   GNU General Public License version 2 or later
+ */
+
+namespace Jyotish\Calendar;
+
+/**
+ * ISKCON's historical events.
+ *
+ * @author Kunjara Lila das <vladya108@gmail.com>
+ */
+class IskconEvents extends \Jyotish\Calendar\Event{
+    static public $eventsTitle = 'ISKCON historical events';
+
+    static public $eventsList = [
+        [
+            self::COL_NAME => 'Shri Jayananda Prabhu (Disappearance)',
+            self::COL_MASA => 2,
+            self::COL_TITHI => 13,
+        ],
+        [
+            self::COL_NAME => 'The incorporation of ISKCON in New York',
+            self::COL_MASA => 5,
+            self::COL_TITHI => 24,
+        ],
+        [
+            self::COL_NAME => 'Shrila Prabhupada departure for the USA',
+            self::COL_MASA => 6,
+            self::COL_TITHI => 16,
+        ],
+        [
+            self::COL_NAME => 'Acceptance of sannyasa by Shrila Prabhupada',
+            self::COL_MASA => 6,
+            self::COL_TITHI => 15,
+        ],
+        [
+            self::COL_NAME => 'Shrila Prabhupada arrival in the USA',
+            self::COL_MASA => 7,
+            self::COL_TITHI => 22,
+        ],
+    ];
+}
