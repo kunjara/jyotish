c98b0b7e653ef2711fee8a21c0a8e494b3c99915
Vladislav Pugachev <vladya108@gmail.com>
2015-04-27 10:16:47 +0400

    Extract Hora class

---
+++ src/Jyotish/Muhurta/Hora.php
@@ -0,0 +1,156 @@
+<?php
+/*
+ * @link      http://github.com/kunjara/jyotish for the canonical source repository
+ * @license   GNU General Public License version 2 or later
+ */
+
+namespace Jyotish\Muhurta;
+
+use Jyotish\Graha\Graha;
+use Jyotish\Ganita\Time;
+use Jyotish\Ganita\Math;
+use Jyotish\Base\Utils;
+use Jyotish\Panchanga\Panchanga;
+use Jyotish\Panchanga\Vara\Vara;
+use DateTime;
+use DateTimeZone;
+
+/**
+ * Hora class.
+ *
+ * @author Kunjara Lila das <vladya108@gmail.com>
+ */
+class Hora {
+    /**
+     * Yama hora
+     */
+    const TYPE_YAMA = 'yama';
+    /**
+     * Kala hora
+     */
+    const TYPE_KALA = 'kala';
+    
+    protected $panchangaObject = null;
+    
+    protected $ganitaData = null;
+    
+    protected $userDateTime = null;
+    
+    protected $userTimeZone = null;
+
+    /**
+     * Constructor
+     * 
+     * @param \Jyotish\Panchanga\Panchanga $Panchanga
+     */
+    public function __construct(Panchanga $Panchanga)
+    {
+        $this->panchangaObject = $Panchanga;
+        $this->ganitaData = $this->panchangaObject->getData();
+        
+        $this->userDateTime = Time::createDateTime($this->ganitaData['user']);
+        $this->userTimeZone = new DateTimeZone($this->ganitaData['user']['timezone']);
+    }
+
+    /**
+     * Get yama hora.
+     * 
+     * @return array
+     */
+    public function getHoraYama()
+    {
+        $risingToday = new DateTime($this->ganitaData['rising'][Graha::KEY_SY][2]['rising'], $this->userTimeZone);
+        $risingTomorrow = new DateTime($this->ganitaData['rising'][Graha::KEY_SY][3]['rising'], $this->userTimeZone);
+        $settingYesterday = new DateTime($this->ganitaData['rising'][Graha::KEY_SY][1]['setting'], $this->userTimeZone);
+        $settingToday = new DateTime($this->ganitaData['rising'][Graha::KEY_SY][2]['setting'], $this->userTimeZone);
+        
+        if($this->userDateTime > $risingToday and $this->userDateTime < $settingToday){
+            $rising = $risingToday;
+            $setting = $settingToday;
+            
+            $intervalHora = ($setting->format('U') - $rising->format('U')) / 12;
+            $intervalTime = $this->userDateTime->format('U') - $rising->format('U');
+            $isDay = true;
+        }else{
+            if($this->ganitaData['user']['date'] == $risingToday->format(Time::FORMAT_DATA_DATE)){
+                $rising = $risingTomorrow;
+                $setting = $settingToday;
+            }else{
+                $rising = $risingToday;
+                $setting = $settingYesterday;
+            }
+            $intervalHora = ($rising->format('U') - $setting->format('U')) / 12;
+            $intervalTime = $this->userDateTime->format('U') - $setting->format('U');
+            $isDay = false;
+        }
+        
+        $number = (int)ceil($intervalTime / $intervalHora);
+        $vara = $this->panchangaObject->getVara();
+        
+        $horaNumber = $isDay ? $number : $number + 12;
+        $riseSet = $isDay ? clone($rising) : clone($setting);
+        $intervalModify = round($intervalHora * $number);
+        $horaEnd = $riseSet->modify("+{$intervalModify} seconds")->format(Time::FORMAT_DATETIME);
+        
+        $hora = [
+            'number' => $horaNumber,
+            'key' => self::getLord($horaNumber, $vara['key']),
+            'interval' => $intervalHora,
+            'left' => fmod($intervalTime, $intervalHora) * 100 / $intervalHora,
+            'end' => $horaEnd,
+        ];
+        
+        return $hora;
+    }
+    
+    /**
+     * Get kala hora.
+     * 
+     * @return array
+     */
+    public function getHoraKala()
+    {
+        $hours = (int)$this->userDateTime->format('G');
+        $horaNumber = Math::distanceInCycle(6, $hours, 24);
+        $weekNumber = $this->userDateTime->format('w');
+        
+        $intervalHora = 3600;
+        $intervalStart = (int)$this->userDateTime->format('i') * 60 + (int)$this->userDateTime->format('s');
+        $intervalEnd = $intervalHora - $intervalStart;
+        
+        if($hours >= 6){
+            $varaNumber = $weekNumber;
+        }else{
+            $varaNumber = $weekNumber != 0 ? $weekNumber - 1 : 6;
+        }
+        
+        $varaKey = array_keys(Vara::$vara)[$varaNumber];
+        
+        $hora = [
+            'number' => $horaNumber,
+            'key' => self::getLord($horaNumber, $varaKey),
+            'interval' => $intervalHora,
+            'left' => fmod($intervalStart, $intervalHora) * 100 / $intervalHora,
+            'end' => $this->userDateTime->modify("+{$intervalEnd} seconds")->format(Time::FORMAT_DATETIME),
+        ];
+        
+        return $hora;
+    }
+    
+    /**
+     * Get lord of hora.
+     * 
+     * @param int $horaNumber Number of hora
+     * @param int $varaKey Key of vara
+     * @return string
+     */
+    static public function getLord($horaNumber, $varaKey)
+    {
+        $lords = Utils::shiftArray(Graha::grahaList(Graha::LIST_CHESHTA), $varaKey);
+        
+        $lordsKeys = array_keys($lords);
+        $numLord = Math::numberInCycle(1, $horaNumber, 7) - 1;
+        
+        return $lordsKeys[$numLord];
+    }
+}
--- src/Jyotish/Muhurta/Muhurta.php
+++ src/Jyotish/Muhurta/Muhurta.php
@@ -6,14 +6,8 @@
 
 namespace Jyotish\Muhurta;
 
-use Jyotish\Graha\Graha;
 use Jyotish\Panchanga\Panchanga;
 use Jyotish\Ganita\Time;
-use Jyotish\Ganita\Math;
-use Jyotish\Base\Utils;
-use DateTime;
-use DateTimeZone;
-use DateInterval;
 
 /**
  * Muhurta class.
@@ -21,15 +15,6 @@ use DateInterval;
  * @author Kunjara Lila das <vladya108@gmail.com>
  */
 class Muhurta {
-    /**
-     * Yama hora
-     */
-    const HORA_YAMA = 'yama';
-    /**
-     * Kala hora
-     */
-    const HORA_KALA = 'kala';
-
     const PANCHAKA_MRITYU = 1;
     const PANCHAKA_AGNI   = 2;
     const PANCHAKA_RAJA   = 4;
@@ -38,6 +23,8 @@ class Muhurta {
     
     protected $panchangaObject = null;
     
+    protected $ganitaData = null;
+
     protected $dateTimeObject = null;
     
     protected $dateTimeObjectStart = null;
@@ -86,57 +73,26 @@ class Muhurta {
      * Get hora. The Vedic system of time division divides each day (from one 
      * sunrise to another) into 24 horas.
      * 
+     * @param string $type Type of hora
      * @return array
      */
-    public function getHora()
+    public function getHora($type = Hora::TYPE_KALA)
     {
-        $userDateTime = Time::createDateTime($this->ganitaData['user']);
-        $userTimeZone = new DateTimeZone($this->ganitaData['user']['timezone']);
+        $Hora = new Hora($this->panchangaObject);
         
-        $risingToday = new DateTime($this->ganitaData['rising'][Graha::KEY_SY][2]['rising'], $userTimeZone);
-        $risingTomorrow = new DateTime($this->ganitaData['rising'][Graha::KEY_SY][3]['rising'], $userTimeZone);
-        $settingYesterday = new DateTime($this->ganitaData['rising'][Graha::KEY_SY][1]['setting'], $userTimeZone);
-        $settingToday = new DateTime($this->ganitaData['rising'][Graha::KEY_SY][2]['setting'], $userTimeZone);
-        
-        if($userDateTime > $risingToday and $userDateTime < $settingToday){
-            $rising = $risingToday;
-            $setting = $settingToday;
-            
-            $intervalTime = $userDateTime->format('U') - $rising->format('U');
-            $intervalHora = ($setting->format('U') - $rising->format('U')) / 12;
-            $isDay = true;
-        }else{
-            if($this->ganitaData['user']['date'] == $risingToday->format(Time::FORMAT_DATA_DATE)){
-                $rising = $risingTomorrow;
-                $setting = $settingToday;
-            }else{
-                $rising = $risingToday;
-                $setting = $settingYesterday;
-            }
-            $intervalTime = $userDateTime->format('U') - $setting->format('U');
-            $intervalHora = ($rising->format('U') - $setting->format('U')) / 12;
-            $isDay = false;
+        switch ($type){
+            case Hora::TYPE_YAMA:
+                // For Polar circle
+                if(abs($this->ganitaData['user']['latitude']) >= 65){
+                    $hora = $Hora->getHoraKala();
+                    break;
+                }
+                $hora = $Hora->getHoraYama();
+                break;
+            case Hora::TYPE_KALA:
+            default:
+                $hora = $Hora->getHoraKala();
         }
-        
-        $number = ceil($intervalTime / $intervalHora);
-        $vara = $this->panchangaObject->getVara();
-        $lords = Utils::shiftArray(Graha::grahaList(Graha::LIST_CHESHTA), $vara['key']);
-        
-        $lordsKeys = array_keys($lords);
-        $numHora = $isDay ? $number : $number + 12;
-        $numLord = Math::numberInCycle(1, $numHora, 7) - 1;
-        
-        $riseSet = $isDay ? clone($rising) : clone($setting);
-        $intervalModify = round($intervalHora * $number);
-        $horaEnd = $riseSet->modify("+{$intervalModify} seconds")->format(Time::FORMAT_DATETIME);
-        
-        $hora = [
-            'number' => $numHora,
-            'key' => $lordsKeys[$numLord],
-            'interval' => $intervalHora,
-            'left' => fmod($intervalTime, $intervalHora) * 100 / $intervalHora,
-            'end' => $horaEnd,
-        ];
         
         return $hora;
     }
@@ -180,7 +136,7 @@ class Muhurta {
     {
         $this->dateTimeObjectStart = Time::createDateTime($this->ganitaData['user']);
         $this->dateTimeObjectEnd = clone($this->dateTimeObjectStart);
-        $this->dateTimeObjectStart->modify('-1 day');
+        $this->dateTimeObjectStart->modify('-28 hours');
         
         $this->panchangaObject->setData([
             'date' => $this->dateTimeObjectStart->format(Time::FORMAT_DATA_DATE),
